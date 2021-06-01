8e48816811f7ceea12d73833a6beba8ef64c31c9
Winston R. Milling <Winston@Milli.ng>
2021-06-01 15:15:57 -0500

    Add some basic templating

--- dot_vim/executable_install.sh
+++ dot_vim/executable_install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Link vimrc to repo
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s "${HOME}/.vim/vimrc" "${HOME}/.vimrc"
 
 # Install zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 # Grab vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 
 # Install all plugins
 vim +PluginInstall +qall
--- dot_zsh/zshrc.d/100.setup-java.sh
+++
@@ -1,3 +0,0 @@
-if [[  "$OSTYPE" == "darwin"* ]]; then
-    export JAVA_HOME=`/usr/libexec/java_home`
-fi
