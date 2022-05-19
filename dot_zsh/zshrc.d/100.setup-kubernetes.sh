aee9796b3487ab054138f1b274d1862964f98c18
Winston R. Milling <Winston@Milli.ng>
2022-05-19 15:18:08 -0500

    K8s: Add removal of duplicate messages
    My cluster is strange sometimes

--- dot_zsh/zshrc.d/100.setup-kubernetes.sh
+++ dot_zsh/zshrc.d/100.setup-kubernetes.sh
@@ -3,4 +3,4 @@ alias kx=kubectx
 alias kn=kubens
 alias ks=kubeseal
 alias kgn="k get nodes -o wide --sort-by=.metadata.name"
-alias kge="k get events -A -w"
+alias kge="k get events -A -w | grep -v \"61m\""
