26ab4b5abad9e7ab1a6954c3d4996026d7a11ab9
Winston R. Milling <Winston@Milli.ng>
2022-08-05 23:15:04 -0500

    NixOS-ifying while sleepy means mistakes

--- dot_zsh/zshrc.d/100.nix-aliases.sh
+++ dot_zsh/zshrc.d/100.nix-aliases.sh
@@ -1,3 +1,3 @@
-nrs=sudo sh -c 'cd /etc/nixos && git pull && nixos-rebuild switch'
-ncg=sudo nix-collect-garbage
-ncgd=sudo nix-collect-garbage -d
+alias nrs='sudo sh -c "cd /etc/nixos && git pull && nixos-rebuild switch"'
+alias ncg="sudo nix-collect-garbage"
+alias ncgd="sudo nix-collect-garbage -d"
