1020b1e9c1fc83b3914f03e59e83556696b1623c
Winston R. Milling <Winston@Milli.ng>
2023-06-17 23:30:57 -0500

    Move to flakes for nixos
    Move back to tmux

--- dot_zsh/zshrc.d/100.nix-aliases.sh
+++ dot_zsh/zshrc.d/100.nix-aliases.sh
@@ -1,3 +1,3 @@
-alias nrs='sudo sh -c "cd /etc/nixos && git pull && nixos-rebuild switch"'
+alias nrs='sudo sh -c "cd /etc/nixos && git pull && nixos-rebuild switch --flake ."'
 alias ncg="sudo nix-collect-garbage"
 alias ncgd="sudo nix-collect-garbage -d"
